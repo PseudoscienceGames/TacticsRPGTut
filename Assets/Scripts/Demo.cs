e03f49a2d8d3a3944458e40e259082eb3245363d
PseudoscienceGames <quade.81@gmail.com>
2018-01-17 00:15:02 -0600

    Finished input controller

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+	private void OnEnable()
+	{
+		InputController.moveEvent += OnMoveEvent;
+		InputController.fireEvent += OnFireEvent;
+	}
+	private void OnDisable()
+	{
+		InputController.moveEvent -= OnMoveEvent;
+		InputController.fireEvent -= OnFireEvent;
+	}
+	void OnMoveEvent(object sender, InfoEventArgs<Point> e)
+	{
+		Debug.Log("Move " + e.info.ToString());
+	}
+	void OnFireEvent(object sender, InfoEventArgs<int> e)
+	{
+		Debug.Log("Fire " + e.info);
+	}
+
+}
