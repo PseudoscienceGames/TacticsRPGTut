eb305e86f856e800d9061539a556d5c38f42ee2f
PseudoscienceGames <quade.81@gmail.com>
2018-01-19 14:48:16 -0600

    Finished conversations

---
+++ Assets/Editor/AssetCreator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class YourClassAsset
+{
+	[MenuItem("Assets/Create/Conversation Data")]
+	public static void CreateConversationData()
+	{
+		ScriptableObjectUtility.CreateAsset<ConversationData>();
+	}
+}
