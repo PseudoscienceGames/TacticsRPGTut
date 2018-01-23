e9dd8890c214abe60aa741e78059ae977328f8e5
PseudoscienceGames <Quade81@gmail.com>
2018-01-23 15:16:17 -0600

    Mid Stats Stat Component

---
+++ Assets/Scripts/Extensions/MaxValueModifier.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxValueModifier : ValueModifier
+{
+	public float max;
+
+	public MaxValueModifier (int sortOrder, float min) : base (sortOrder)
+	{
+		this.max = max;
+	}
+
+	public override float Modify(float value)
+	{
+		return Mathf.Max(value, max);
+	}
+}
